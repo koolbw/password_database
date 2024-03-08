b45147861c95290921cb66325a7d2d9784a5ca43
koolbw <koolbw2@gmail.com>
2024-03-08 07:39:41 -0700

    changes to main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import firebase_admin
+from firebase_admin import credentials
 from firebase_admin import firestore
 
-# Application Default credentials are automatically created.
-app = firebase_admin.initialize_app()
+cred = credentials.Certificate("key.json")
+firebase_admin.initialize_app(cred)
 db = firestore.client()
+
 
 doc_ref = db.collection("users").document("alovelace")
 doc_ref.set({"first": "Ada", "last": "Lovelace", "born": 1815})
