0a51bc8dcea9d2f112c761d7c83353787b74b4ce
koolbw <koolbw2@gmail.com>
2024-03-08 08:22:15 -0700

    added file for testing encryption

---
+++ enryption_test.py
@@ -0,0 +1,37 @@
+# Symmetric key encryption 
+
+# from cryptography.fernet import Fernet
+
+# message = "password123"
+
+# key = Fernet.generate_key()
+# fernet = Fernet(key)
+# encMessage = fernet.encrypt(message.encode()) 
+
+# print("original string: ", message)
+# print("encrypted string: ", encMessage)
+
+# decMessage = fernet.decrypt(encMessage).decode()
+ 
+# print("decrypted string: ", decMessage)
+
+
+# Asymmetric key encryption
+
+import rsa
+
+publicKey, privateKey = rsa.newkeys(512)
+
+message = "thisIsMyPassword123!"
+
+
+encMessage = rsa.encrypt(message.encode(), 
+						publicKey)
+
+print("original string: ", message)
+print("encrypted string: ", encMessage)
+
+
+decMessage = rsa.decrypt(encMessage, privateKey).decode()
+
+print("decrypted string: ", decMessage)
